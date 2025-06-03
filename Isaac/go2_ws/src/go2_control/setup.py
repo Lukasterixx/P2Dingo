c2bed73c07d5764fae6955dcdf7578047f4b9fdd
Lukasterixx <lukas.nielsen@iinet.net.au>
2025-06-03 18:05:55 +0200

    another gitignore fix

---
+++ Isaac/go2_ws/src/go2_control/setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+package_name = 'go2_control'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='lukas',
+    maintainer_email='lukas@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        	'walk_forward = go2_control.walk_forward:main',
+        ],
+    },
+)
